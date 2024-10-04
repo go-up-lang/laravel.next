0481ed6bcf0a5e9310750a1d1080000409dd9e27
go-up-lang <dbsrbtjs@gmail.com>
2024-10-04 17:49:53 +0900

    two backend

---
+++ app/Http/Controllers/WriteController.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class WriteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'note' => 'required|string|max:255', // note 필수 및 문자열, 최대 255자
+        ]);
+    
+        $note = $request->input('note');
+    
+        // note 값을 덤프하고 요청을 종료
+        dd($note);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::post('/write', [WriteController::class, 'store'])
+                ->middleware('guest')
+                ->name('write');
